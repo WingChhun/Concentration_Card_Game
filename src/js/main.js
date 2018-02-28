0a7d7835c04a618bb5c2ffc1875a25c977428864
James Chhun <capt.wing.chhun@gmail.com>
2018-02-27 18:39:39 -0800

    Refactored some code, mainly to fix flip toggle

--- src/js/main.js
+++ src/js/main.js
@@ -3,23 +3,27 @@ let cardArr = [];
 
 
 $(document).ready(function () {
+    console.log("Document loaded and ready...");
     const $btnNew = document.querySelector("#btn__new-game");
-
-    const $loadSpinner = $(".loader");
-    window.addEventListener("load", function (e) {
-        $loadSpinner.css({
-            'display': 'none'
-        });
-    });
 
     $btnNew.addEventListener("click", startGame);
     start();
 });
 
 function start() {
-    console.log("Document loaded and ready...");
-
+    loadSpinner();
     setDeck();
+
+}
+
+function loadSpinner() {
+    const $loadSpinner = $(".loader");
+
+    window.addEventListener("load", function (e) {
+        $loadSpinner.css({
+            'display': 'none'
+        });
+    });
 
 }
 
@@ -30,7 +34,7 @@ function startGame(e) {
             -String templlate must have embedded data attr for data-value, to allow for pattern matching
     */
     e.preventDefault(); //prevent a page Refresh
-    console.log("User has clicked start game!");
+    // console.log("User has clicked start game!");
     setDeck();
 
 }
@@ -46,12 +50,16 @@ function setDeck() {
         .done((data) => {
 
             Deck = data;
-            console.log("Deck success", Deck);
+            $(".u-alert--success-2").toggleClass("success-shown");
+            setTimeout(() => {
+                $(".u-alert--success-2").toggleClass("success-shown, hidden");
+            }, 500);
+            // console.log("Deck success", Deck);
             setCards();
         })
         .fail((err) => {
             alert("Error getting New deck!");
-            console.log("Error", err);
+            // console.log("Error", err);
         });
 
 };
@@ -73,12 +81,12 @@ function setCards() {
             results.cards.forEach((card) => {
                 cardArr.push(card);
             });
-            console.log("Card success", cardArr);
+            // console.log("Card success", cardArr);
             //After, run outer functions
             populateCards(); //go to other function to continue
         })
         .fail((err) => {
-            console.log("Error returning Cards...", err);
+            // console.log("Error returning Cards...", err);
         });
 };
 
@@ -101,12 +109,12 @@ function createTemplate(strTemplate, cardElement) {
         <div class="Card__item"  id="card__flip" data-value ="${cardElement.value}" data-suit ="${cardElement.suit}" data-code ="${cardElement.code}" data-flipped="false">
             <div class="front">
             <img src="./assets/playing-card-back.png" class="Card__img" alt="card front">
-            </div>
+            </div><!-- end front -->
             <div class="back">
-            <img src="${cardElement.image}" class="Card__img" alt="card-front">
-  
-            </div>
-        </div>`;
+            <img src="${cardElement.image}" class="Card__img" alt="card-front">            
+            
+            </div><!-- end back -->
+        </div><!-- end card__item -->`;
     return strTemplate;
 };
 
@@ -140,10 +148,10 @@ function checkCards(SelectedCard) {
         $cardArr.push(element);
     }
 
-    console.log("Flipped array lenmgth", $flippedCards.length);
+    // console.log("Flipped array length", $flippedCards.length);
     if ($flippedCards.length == 2) {
         //2 cards are shown, now check
-        console.log("Performing action...");
+        // console.log("Performing action...");
         let $firstCard = $flippedCards[0];
         let $secondCard = $flippedCards[1];
 
@@ -151,47 +159,56 @@ function checkCards(SelectedCard) {
             //Remove the cards from the array
             //Only return if they do not match the value AND dont match the respective codes
             //Show success alert
-            $(".alert-success").toggleClass("success-shown");
-            $firstCard.remove();
-            $secondCard.remove();
+            $(".u-alert--success").toggleClass("success-shown");
+            //Timeout, Add a delay so user can see that they have a match
+            setTimeout(() => {
+                $firstCard.remove();
+                $secondCard.remove();
+            }, 450);
+
             let newArr = cardArr.filter((element) => {
                 return element.code != $firstCard.dataset.code && element.code != $secondCard.dataset.code;
             });
             cardArr = newArr;
             setTimeout(() => {
-                $(".alert-success").toggleClass("success-shown");
+                $(".u-alert--success").toggleClass("success-shown");
                 //User has completed the game, toggle success
                 if (cardArr.length == 0) {
                     $('.toggle-success').toggleClass('hidden, success-shown');
                 }
-            }, 600);
+            }, 500);
 
         } else if (!($firstCard.dataset.value == $secondCard.dataset.value)) {
-            /*
-             - The cards do not match,
-             -Empty the flippedArray, to be reusable
-             - toggle all 
 
-                -Timeout, allow user to see cards before flipping
-            -Show mismatch error
-            */
-            $(".alert-danger").toggleClass("success-shown");
-            console.log("The cards do not match, handling it...");
+            // console.log("The cards do not match, handling it...");
+
+            $(".u-alert--danger").toggleClass("success-shown");
+            setTimeout(() => {
+                this.flipActive();
+            }, 500);
+
             $flippedCards == 0;
 
-            setTimeout(() => {
-                $(".active-flipped").flip('toggle');
-                $flippedCards.forEach((element) => {
-                    element.classList.toggle("active-flipped");
-                })
-                $(".alert-danger").toggleClass("success-shown");
-            }, 700);
 
+        } //end else if datasets dont match
 
-        }
+    } //end initial if
 
+    this.flipActive = function () {
+        /*
+         - The cards do not match,
+         -Empty the flippedArray, to be reusable
+         - toggle all 
+            -Timeout, allow user to see cards before flipping
+        -Show mismatch error
+        */
+        //clearTimeout(); //clear timeout, in case user clicks too fast
+        $(".active-flipped").flip('toggle');
+        $flippedCards.forEach((element) => {
+            element.classList.remove("active-flipped");
+        })
+        $(".u-alert--danger").toggleClass("success-shown");
 
 
     }
-
-};
+}; //end function
